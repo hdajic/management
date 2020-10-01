17b9e295250609ee45fb1a00ef40d493cfb01aa8
Haris Dajic <haris.dajic@hpe.com>
2020-10-01 12:23:02 +0200

    Fixed all errors

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,26 +3,16 @@ import { Route, Switch } from "react-router-dom";
 import Products from "./products";
 import CreateProduct from "./createProduct";
 import EditProduct from "./editProduct";
-import ProductDetails from "./productDetails";
 
 class Main extends Component {
   render() {
     return (
       <div className="container">
         <Switch>
-          <Route
-            path="/product/create"
-            render={() => (
-              <CreateProduct
-                onCreateProduct={this.handleProductCreate}
-                showDialog={true}
-              />
-            )}
-          />
+          <Route path="/product/create" component={CreateProduct} />
           <Route path="/product/edit/:id" component={EditProduct} />
-          <Route path="/product/details/:id" component={ProductDetails} />
-          <Route path="/products"  component={Products} />
-          <Route path="/" exact  component={Products} />
+          <Route path="/products" component={Products} />
+          <Route path="/" exact component={Products} />
         </Switch>
       </div>
     );
--- src/components/productDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class ProductDetails extends Component {
-  render() {
-    return <h1>Product Details {this.props.match.params.id}</h1>;
-  }
-}
-
-export default ProductDetails;
